a651855726c2a344bb6ccee027bf765a57c66421
hareemzaf <150966998+hareemzaf@users.noreply.github.com>
2023-12-10 04:24:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+//  example1
+// const FirstName = "hareem";
+// let lastName = "Zafar";
+// if(FirstName === "hareem"&& lastName === "Zafar") {
+//     console.log("you're not allowed")
+// }
+// else{
+// console.log("you're allowed")
+// }
+// exmple2
+// const FirstName = "hareem";
+// let lastName = "Zafar";
+// let secondperson = "hifza";
+// if(FirstName === "hareem" || lastName === "Zafar") {
+//     console.log("you're not allowed")
+// }
+// else{
+// console.log("you're allowed")
+// }
+// ammar
+// function greet(){
+//     console.log("hello hareem")
+// }
+// // mustufa
+// function sum(){
+//     console.log(2+2);
+// }
+// // fezan
+// function tranding(){
+//     console.log("moye moyee");
+// }
+// greet();
+// greet();
+// sum();
+// greet();
+// greet();
+// tranding();
+// parameter or argument just like a pipe
+// ammar
+// function greet(name:string){
+//     console.log("hello", name);
+// }
+// greet("hareem");
+// greet("hifza");
+// mustufa  
+// function sum(num1:number,num2:number){
+// console.log(num1+num2)
+// }
+// sum(5, 10);
+// sum example2
+// function sum(num1:number,num2:number){
+//     console.log(num1+num2)
+//     return num1 + num2;
+//     }
+//     let ans = sum(5,10);
+//     function multiply(num1:number){
+//         console.log(num1 * 2)
+//         }
+//         multiply()
+// Areey practices
+// let fruit1 = "apple";
+// let fruit2 = "orange";
+// let fruit3 = "apple";
+// let fruit4 = "apple";
+// let fruits = ["apple","banana", "orange","mango"];
+// let subjects: string[] = ["math","urdu", "english","pyhsic"];
+// let ages:number[] = [1,2,3,4,5,6]
+// let a = [true,false,true]
+// example
+// let fruits = ["apple","banana", "orange","mango"];
+// console.log(fruits[2],fruits[3]);
+// let menu = ["kofta", "kheer", "biryani", "gajer ka hulwa"];
+// menu.pop();
+// menu.push("chicken tikka", "chicken karhai");
+// console.log(menu);
+var menu = ["kofta", "kheer", "biryani", "gajer ka hulwa"];
+var myFirstPopped = menu.pop();
+console.log(myFirstPopped);
+// let pushedreturn = menu.push("chicken tikka ","chicken karahi");
+// console.log(pushedreturn);
+// menu.shift();
+// menu.unshift();
+// const fruits =["apple","banana", "orange","mango"];
+// const citrus = fruits.slice(1,3);
+// console.log(citrus);
